d01051ab58c5da1a0f4949198724998b953b40da
jorge.amado <darksoul.uci@gmail.com>
2016-08-13 17:53:53 -0300

    moved the sustantivos.txt to the test resources

--- facundia/src/test/java/org/jasr/facundia/rules/WordListExtract3.java
+++ facundia/src/test/java/org/jasr/facundia/rules/WordListExtract3.java
@@ -2,6 +2,8 @@ package org.jasr.facundia.rules;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
@@ -13,9 +15,7 @@ public class WordListExtract3 {
 
     public static void main(String[] args) throws Exception {
 
-        File file = new File("C:/Users/jorge.amado/Desktop/Inflector/sustantivos.txt");
-
-        LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+        LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream("src/test/resources/sustantivos.txt"),"UTF-8"));
 
         List<String> singulars = new ArrayList<String>();
         List<String> plurals = new ArrayList<String>();
