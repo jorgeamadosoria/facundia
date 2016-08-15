f8f9fb153c6c2ac99a98c7f1117ee07a98184027
jorge.amado <darksoul.uci@gmail.com>
2016-08-15 09:21:43 -0300

    first version of DFA for syllabe division. It is functional but not ready, though

--- facundia/src/test/java/org/jasr/facundia/rules/WordListExtract3.java
+++ facundia/src/test/java/org/jasr/facundia/rules/WordListExtract3.java
@@ -1,26 +1,25 @@
 package org.jasr.facundia.rules;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jasr.facundia.inflector.InflectionResult;
 import org.jasr.facundia.inflector.rules.RuleChain;
 
 public class WordListExtract3 {
 
     public static void main(String[] args) throws Exception {
 
-        LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream("src/test/resources/sustantivos.txt"),"UTF-8"));
+        LineNumberReader reader = new LineNumberReader(
+                new InputStreamReader(new FileInputStream("src/test/resources/sustantivos.txt"), "UTF-8"));
 
         List<String> singulars = new ArrayList<String>();
         List<String> plurals = new ArrayList<String>();
-        
-                 while (reader.ready()) {
+
+        while (reader.ready()) {
             String[] value = reader.readLine().split(" ");
             singulars.add(value[0]);
             // System.out.println(value[0]);
@@ -29,10 +28,12 @@ public class WordListExtract3 {
         reader.close();
 
         RuleChain rule = new RuleChain();
-        for (int i = 0; i < singulars.size(); i++){
-            String plural = rule.apply(singulars.get(i)).getPlural();
-            if (!plurals.get(i).equals(plural))
-            System.out.println(plurals.get(i) + " " + plural);
+        for (int i = 0; i < singulars.size(); i++) {
+            InflectionResult result = rule.apply(singulars.get(i));
+            if (!plurals.get(i).equals(result.getPlural()))
+                System.out.println(plurals.get(i) + " " + result.getPlural());
+            else
+                System.out.println(result.getPlural() + " " + result.getRuleApplied().getDescription());
         }
 
     }
